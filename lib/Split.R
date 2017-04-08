f5fdbfdcf6cb4dadb88ca213289ee2539eca5a56
Ruxue Peng <rp2815@columbia.edu>
2017-04-08 18:45:10 -0400

    Add files via upload

---
+++ lib/Split.R
@@ -0,0 +1,11 @@
+Train_Half = function(Mat){
+  rownumber = round(nrow(Mat)/2)
+  return(Mat[1:rownumber,])
+}
+Test_Half = function(Mat){
+  rownumber = round(nrow(Mat)/2)
+  return(Mat[(rownumber+1):nrow(Mat),])
+}
+
+Split_Train = function(DataMat){ddply(DataMat,.(DataMat$AuthorID),Train_Half)[,-1]}
+Split_Test = function(DataMat){ddply(DataMat,.(DataMat$AuthorID),Test_Half)[,-1]}
