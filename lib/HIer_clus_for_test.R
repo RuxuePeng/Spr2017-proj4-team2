8fa563bccab93a93876ed9bda0b3e0faf4b01f02
jz2773 <jz2773@columbia.edu>
2017-04-10 12:21:03 -0400

    modified for testset

---
+++ lib/HIer_clus_for_test.R
@@ -0,0 +1,28 @@
+# iterate over hierarchial clustering levels
+
+Hier_clus_test= function(Fea_wl_test,Fea_wol_test,Lamda = best_lamda,data = Test[[1]]){
+  #hard start
+  lamda = Lamda
+  N = (max(data$AuthorID)+2)
+  init_cluster = kmeans(Fea_wl,centers = N,nstart =1)
+  G = get_GoldStand(data)
+  y = G$y
+  a_c =G$a_c
+  b_d = G$b_d 
+  # initialization
+  label_i= init_cluster$cluster
+  
+  for (v in 1: (N -length(unique(y)))){
+    labels= get_labels(init_label= label_i)
+    
+      score <- aaply(labels,2,get_score,lamda)
+      # extract the best score and N_hat and its F1 score
+      ind_N_hat <- which.max(score)
+      #ind_N_star <- which(F1==max(F1))
+      label_i <-labels[,ind_N_hat]
+      
+  }
+  F1_N_hat = get_F1(label_i,A_C = a_c,B_D = b_d)
+  return(list(label_i,F1_N_hat))
+}
+
