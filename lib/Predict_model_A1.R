a0a5b280edbe4a070d7f7a25bfa5182e420683e1
xuanzix <xuanz.xu@gmail.com>
2017-04-10 18:05:14 -0400

    predict label use A1

---
+++ lib/Predict_model_A1.R
@@ -0,0 +1,55 @@
+# This function is used for prediction the label
+# Input is a citation which just contains coauthors
+# Output is label, 0 means given citation doesn't contain any coauthors 
+
+citation_A1<-function(citation_A1){
+  test_i<-citation_A1
+  test_i<-strsplit(test_i,split = ";")
+  test_i<-as.data.frame(matrix(unlist(test_i)),stringsAsFactors = FALSE)
+  test_i<-as.data.frame(apply(test_i,2,function(x)gsub('\\s+','',x)),stringsAsFactors = FALSE)
+  test_i<-as.data.frame(test_i[!test_i$V1 == "AKumar",],stringsAsFactors = FALSE)
+  colnames(test_i)<-"coauthor"
+  #class(test1)
+  
+  if (nrow(test_i) == 0 ){
+    predict_label<-0
+  }else{
+    # extract the number of times xi coauthors with A1k
+    seen_df_i<-matrix(NA, nrow = nrow(seen_df),ncol = nrow(test_i))
+    colnames(seen_df_i) <- test_i$coauthor
+    for(i in 1:ncol(seen_df_i)){
+      if(test_i[i,1] %in% colnames(seen_df)){
+        seen_df_i[,i]<-seen_df[,colnames(seen_df)==test_i[i,1]]
+      }else{
+        seen_df_i[,i]<-rep(0,nrow(seen_df))
+      }
+    }
+    
+    # Calculate P(A1k|xi)
+    P_A1_x<-matrix(NA,nrow=n,ncol = ncol(seen_df_i))
+    for(i in 1:n){
+      for (j in 1: ncol(seen_df_i)){
+        if(nxi_seen[i] !=0){
+          P_A1k_seen_coxi<-seen_df_i[i,j]/nxi_seen[i]
+          P_A1_x[i,j] <- P_A1k_seen_coxi*seen_term[i] +unseen_term[i]
+        }else{
+          P_A1_x[i,j] <- unseen_term[i]
+        }
+      }
+    }
+    # Calculate P(Xi|C)
+    P_Xi_c<-rep(NA,length=n)
+    for(i in 1: n){
+      LOG<-0
+      for(j in 1:ncol(P_A1_x)){
+        LOG<-LOG+log(P_A1_x[i,j])
+      }
+      P_Xi_c[i]<-LOG + log(p_xi[i])
+    }
+    
+    # Predict xi
+    predict_label<-names(nxi_seen)[which.max(P_Xi_c)]
+  }
+  
+  return(predict_label)
+}
