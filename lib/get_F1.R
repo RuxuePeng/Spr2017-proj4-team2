8f53be49109f078502b22c3c70920d14167aa5dd
jt2929 <jt2929@columbia.edu>
2017-04-12 23:28:14 -0400

    Add files via upload

--- lib/get_F1.R
+++ lib/get_F1.R
@@ -4,6 +4,16 @@
 #NOTE: need to have: Data as list first,each list contain info about 1 citation; 
 #                    y as the true label vector to compute a+c and b+d
 #######################################################################################
+Fun1 = function(R,y,init_label){
+  a_b <- 0
+  a = 0
+  q1 <- R[1]
+  q2 <- R[2]
+  a = init_label[q1]==init_label[q2]
+  a_b = (init_label[q1]==init_label[q2])&(y[q1]==y[q2])
+  return(c(a = a, a_b = a_b))
+}
+
 get_F1 = function(init_label,data,A_C = a_c,B_D = b_d){
   #pairlist: all the possible pair assignments of all the articles
   #pairnumber: number of all the possible pair assignments
@@ -16,13 +26,14 @@ get_F1 = function(init_label,data,A_C = a_c,B_D = b_d){
   ## compute match/mismatch of this partition method
     a_b <- 0
     a = 0
-    for(k in 1:nrow(pairlist)){
-      q1 <- pairlist[k,1]
-      q2 <- pairlist[k,2]
-      #a_b means a+b
-      a_b <- a_b+(init_label[q1]==init_label[q2])
-      a <- a + ((init_label[q1]==init_label[q2])&(y[q1]==y[q2]))
-    }
+    # PARALLEL PROCESSING
+    cores <- detectCores()-1
+    cl2 = makeCluster(cores,type = "FORK")
+    df = parApply(cl2,pairlist,1,Fun1,y,init_label)
+    stopCluster(cl2)
+    
+    a = rowSums(df)[1]
+    a_b = rowSums(df)[2]
     c_d <- pairnumber-a_b
     ## compute the precision,recall and F1 score of this partition method
     precision = a/a_b
