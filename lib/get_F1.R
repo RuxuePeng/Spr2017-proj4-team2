ed17dc802fa02878c0553f3361a377034c701aa4
jt2929 <jt2929@columbia.edu>
2017-04-13 02:19:01 -0400

    Add files via upload

--- lib/get_F1.R
+++ lib/get_F1.R
@@ -9,8 +9,8 @@ Fun1 = function(R,y,init_label){
   a = 0
   q1 <- R[1]
   q2 <- R[2]
-  a = init_label[q1]==init_label[q2]
-  a_b = (init_label[q1]==init_label[q2])&(y[q1]==y[q2])
+  a_b = init_label[q1]==init_label[q2]
+  a = (init_label[q1]==init_label[q2])&(y[q1]==y[q2])
   return(c(a = a, a_b = a_b))
 }
 
@@ -24,24 +24,24 @@ get_F1 = function(init_label,data,A_C = a_c,B_D = b_d){
   pairnumber = nrow(pairlist)
   
   ## compute match/mismatch of this partition method
-    a_b <- 0
-    a = 0
-    # PARALLEL PROCESSING
-    cores <- detectCores()-1
-    #cl2 = makeCluster(cores,type = "FORK")
-    #df = parApply(cl2,pairlist,1,Fun1,y,init_label)
-    df = apply(cl2,pairlist,1,Fun1,y,init_label)
-    #stopCluster(cl2)
-    
-    a = rowSums(df)[1]
-    a_b = rowSums(df)[2]
-    c_d <- pairnumber-a_b
-    c = A_C - a
-    d = c_d - c
-    accu = (a+d)/(a_b + c_d)
-    ## compute the precision,recall and F1 score of this partition method
-    precision = a/a_b
-    recall = a/A_C
-    F1 = (2*precision*recall)/(precision+recall)   
-   return(F1)
+  a_b <- 0
+  a = 0
+  # PARALLEL PROCESSING
+  cores <- detectCores()-1
+  #cl2 = makeCluster(cores,type = "FORK")
+  #df = parApply(cl2,pairlist,1,Fun1,y,init_label)
+  df = apply(pairlist,1,Fun1,y,init_label)
+  #stopCluster(cl2)
+  
+  a = rowSums(df)[1]
+  a_b = rowSums(df)[2]
+  c_d <- pairnumber-a_b
+  c = A_C - a
+  d = c_d - c
+  accu = (a+d)/(a_b + c_d)
+  ## compute the precision,recall and F1 score of this partition method
+  precision = a/a_b
+  recall = a/A_C
+  F1 = (2*precision*recall)/(precision+recall)   
+  return(F1)
 }
