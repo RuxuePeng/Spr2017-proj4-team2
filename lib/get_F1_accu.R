25253142879dc27a685cfbdf479053e18286ae33
jt2929 <jt2929@columbia.edu>
2017-04-13 00:22:24 -0400

    Update get_F1_accu.R

--- lib/get_F1_accu.R
+++ lib/get_F1_accu.R
@@ -27,10 +27,11 @@ get_F1_accu = function(init_label,data,A_C = a_c,B_D = b_d){
     a_b <- 0
     a = 0
     # PARALLEL PROCESSING
-    cores <- detectCores()-1
-    cl2 = makeCluster(cores,type = "FORK")
-    df = parApply(cl2,pairlist,1,Fun1,y,init_label)
-    stopCluster(cl2)
+    #cores <- detectCores()-1
+    #cl2 = makeCluster(cores,type = "FORK")
+    #df = parApply(cl2,pairlist,1,Fun1,y,init_label)
+    df = apply(cl2,pairlist,1,Fun1,y,init_label)
+    #stopCluster(cl2)
     
     a = rowSums(df)[1]
     a_b = rowSums(df)[2]
