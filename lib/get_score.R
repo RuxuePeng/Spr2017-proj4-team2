885b784d07bd1da8659e28d50571dd1bcccd7355
Ruxue Peng <rp2815@columbia.edu>
2017-04-14 21:44:43 +0800

    Delete get_score.R

--- lib/get_score.R
+++
@@ -1,6 +0,0 @@
-#function for computing score, input is dataframe "labels"
-get_score <- function(label_i,lamda, Fea_wol){
-     Fea_wol_1 <- data.frame(cbind(Fea_wol,label_1=label_i))
-     Fea_wol_1 <- aggregate(Fea_wol_1,by=list(Fea_wol_1$label_1),FUN=mean)
-     score <- sum((as.matrix(Fea_wol_1[,-c(1,ncol(Fea_wol_1))])%*%lamda))
- }
